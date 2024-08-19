51d3d01b4935d34977113248a6a08b9cfbec69f5
rickmoralis <rickmoralis@rickmoraliss-iMac.local>
2024-08-19 16:00:20 +0300

    routes

--- src/app/api/block/route.js
+++ src/app/api/block/route.js
@@ -1,12 +1,23 @@
-import {prismaClient as prisma} from "../../../../prisma/client";
-
-import {NextResponse} from "next/server";
+import { prismaClient as prisma } from "../../../../prisma/client";
+import { NextResponse } from "next/server";
 
 export async function GET() {
-    const blockusers = await prisma.blocked.findMany({
-        orderBy: {
-            createdAt: 'desc'
-        }
-    })
-    return NextResponse.json(blockusers)
+    try {
+        const blockusers = await prisma.blocked.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+        return NextResponse.json(blockusers);
+    } catch (error) {
+        console.error('Error fetching blocked users:', error);
+
+        // Возвращаем ответ с ошибкой
+        return NextResponse.json(
+            { message: 'Internal Server Error' },
+            { status: 500 }
+        );
+    } finally {
+        await prisma.$disconnect(); // Закрываем соединение с базой данных
+    }
 }
--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,14 +1,25 @@
-// pages/api/users.js
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
 
-import {prismaClient as prisma} from "../../../../prisma/client";
-
-import {NextResponse} from "next/server";
+const prisma = new PrismaClient();
 
 export async function GET() {
-    const users = await prisma.user.findMany({
-        orderBy: {
-            createdAt: 'desc'
-        }
-    })
-    return NextResponse.json(users)
+    try {
+        const users = await prisma.user.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+        return NextResponse.json(users);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+
+        // Возвращаем ответ с ошибкой
+        return NextResponse.json(
+            { message: 'Internal Server Error' },
+            { status: 500 }
+        );
+    } finally {
+        await prisma.$disconnect(); // Закрываем соединение с базой данных
+    }
 }
