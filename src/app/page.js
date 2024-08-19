f43379be3fb075175082256a51136a2fc01e526d
rickmoralis <rickmoralis@rickmoraliss-iMac.local>
2024-08-19 20:40:07 +0300

    version 1.0

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,14 @@ import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/post
 
 
 export default async function Home() {
-    const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/users`, { next: { revalidate: 300 } });
-    const res = await data.json()
+    const dataUsers = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/users`, {next: {revalidate: 300}});
+    const resUsers = await dataUsers.json()
+
+    const dataBlocked = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/block`, {next: {revalidate: 300}});
+    const resBlocked = await dataBlocked.json()
     return (
         <>
-            <Main users={res}/>
+            <Main users={resUsers} blocked={resBlocked}/>
         </>
     );
 }
