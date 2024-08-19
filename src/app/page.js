d101eda949e4b3264d61e0eeee219d96fe067808
rickmoralis <rickmoralis@rickmoraliss-iMac.local>
2024-08-19 17:45:01 +0300

    fix revalidate

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Main from "@/app/Main";
 
-export default function Home() {
-    
+
+export default async function Home() {
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/users`, {revalidate: 600})
+    const data = await res.json()
+    console.log(data);
     return (
         <>
             <Main/>
